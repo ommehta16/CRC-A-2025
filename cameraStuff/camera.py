ef02d014abec297040b248eec1a8b09e0e8d393a
derek_z <derekzhang008@gmail.com>
2025-01-23 16:19:50 -0500

    rapid picture test 1

--- cameraStuff/camera.py
+++ cameraStuff/camera.py
@@ -1,9 +1,20 @@
 from picamzero import Camera
 from time import sleep
 
+home_dir = os.environ['HOME'] #set the location of your home directory
+
 cam = Camera()
 cam.start_preview() #starts camera preview
 
+"""
+#VIDEO
 cam.start_preview()
-cam.take_photo(f"{home_dir}/Desktop/piImageTest1.jpg") #save the image to your desktop
+cam.record_video(f"{home_dir}/Desktop/new_video.mp4", duration=5)
 cam.stop_preview()
+"""
+
+#RAPID PICTURES test 
+for i in range(3): #use while true loop later
+    cam.take_photo(f"{home_dir}/Desktop/new_image.jpg") #save the image to your desktop
+    print("Captured Image "+i)
+    sleep(0.5) #seconds
