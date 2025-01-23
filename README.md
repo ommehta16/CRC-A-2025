890fcec4118f9f755964613e164f639a5ffce363
Om Mehta <ommehta16@icloud.com>
2025-01-23 16:34:40 -0500

    make local camera stuff

--- README.md
+++ README.md
@@ -3,7 +3,9 @@ all things for CRC Amogus in 2025
 <h1>camera</h1>
 idk what we do here lol
 
-
+<h1>DEPENDENCIES</h1>
+ - opencv: `pip install opencv-python`
+ - picamzero: `pip install picamzero`
 sus
 skibidi
 hkjefd
--- cameraStuff/main.py
+++ cameraStuff/main.py
@@ -1,5 +1,21 @@
-def main():
-    # do image processing
-   return 
+import cv2
+import time
+from matplotlib import pyplot as plt
 
+
+def main():
+    cam = cv2.VideoCapture(0)
+   
+    res,img = cam.read()
+    for _ in range(1000):
+        if res: break
+        time.sleep(0.01)
+        res,img = cam.read()
+    print(res)
+    img = img[:,:,::-1]
+    plt.imshow(img,interpolation="bicubic")
+    plt.xticks([]); plt.yticks([])
+    plt.show()
+
+    # do image processings
 if __name__ == "__main__": main()
