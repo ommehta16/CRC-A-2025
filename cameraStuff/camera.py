835807c42f5aff00eee34b748c9dd82957e5188c
derek_z <derekzhang008@gmail.com>
2025-01-23 16:06:31 -0500

    test 2

--- cameraStuff/camera.py
+++ cameraStuff/camera.py
@@ -2,6 +2,10 @@ from picamzero import Camera
 from time import sleep
 
 cam = Camera()
-cam.start_preview()
+cam.start_preview() #starts camera preview
 
-sleep(10)
+cam.start_preview()
+cam.take_photo(f"{home_dir}/Desktop/piImageTest1.jpg") #save the image to your desktop
+cam.stop_preview()
+
+
