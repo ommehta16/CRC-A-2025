946ca8199cfd19a072a26ddc08f2eb0c06c28b24
derek_z <derekzhang008@gmail.com>
2025-01-30 16:33:01 -0500

    updated taking pictures thing to image list now

--- cameraStuff/PiCam.py
+++ cameraStuff/PiCam.py
@@ -5,21 +5,12 @@ import numpy as np
 from time import sleep
 import os
 
-def getFrame() -> np.ndarray:
-    # Derek pls implement this
-
-    # It just has to take a picture and save it to some inbuilt data type (NOT a file pls)
-    # return that datatype once your done
-    
-    # thanks :)
-    
-    return np.zeros([1,1])
-    
+from PIL import Image #python imaging library
+import oi #file input stuff
 
 
 
-
-home_dir = os.environ['HOME'] #set the location of your home directory
+#home_dir = os.environ['HOME'] #set the location of your home directory
 
 cam = Camera()
 cam.start_preview() #starts camera preview
@@ -30,9 +21,24 @@ cam.start_preview() #starts camera preview
 pictureDelayInterval = 0.1 #IN SECONDS 
 numberPicturesTaken = 3 #take 3 pictures
 
+#list of images
+imageList = []
+
 #take a bucnh of pictures:
 for i in range(numberPicturesTaken): #use while true loop later
-    cam.take_photo(f"{home_dir}/Desktop/new_image"+str(i)+".jpg") #save the image to your desktop
+
+    #cam.take_photo(f"{home_dir}/Desktop/new_image"+str(i)+".jpg") #save the image to your desktop
+    
+    memoryStream = io.BytesIO()
+    cam.capture(memoryStream, format = 'jpeg')
+    memoryStream.seek(0)
+
+    #convert image to np array
+    image = Image.open(stream)
+    imageArray = np.array(image) #converts image to array
+
+    imageList.append(imageArray)
+    
     print("Captured Image: "+str(i))
     sleep(pictureDelayInterval) #seconds
 
