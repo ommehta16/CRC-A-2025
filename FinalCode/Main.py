a71f05e77e622c861e7e1d1e5fbe70266c56f524
Om Mehta <ommehta16@icloud.com>
2025-05-15 21:06:27 -0400

    fix the merge conflict, actually

--- FinalCode/Main.py
+++ FinalCode/Main.py
@@ -1,16 +1,11 @@
 '''
 The algorithim at its core, as pseudocode for now
-<<<<<<< HEAD
 '''
 import time
 from multiprocessing import Process, Queue, Event, set_start_method
 from Camera import producer
 from DetectRectColor import detector, consumer
-=======
-
-DONT USE THIS FILE
->>>>>>> refs/remotes/origin/main
-
+'''
 PLS PUT STUFF IN CRC-A-2025/main.py
 plsplsplps
 
@@ -20,17 +15,6 @@ the point of having git is that we don't need to version control with copies
 dslafhjlsahjfkhdsakjhfkaj
 '''
 #everything should go in a try finally, with cleanup processes in the except keyboard interrupt
-if __name__ == "__main__":
-    #initialize everything, camera, pins, processes etc
-    #run through ALL sensors
-    #if camera sees something:
-        #turn on lights, dispense, disable this function until a certain distance is traveled
-    #if camera sees nothing:
-        #is there anything significant on the color sensor OR frontfacing distance? if so, react
-        #react to sideways wall distance
-    #move
-    #add movement to mapping, loop
-'''
 
 if __name__ == "__main__":
     
--- movement.py
+++ movement.py
@@ -23,6 +23,15 @@ motorB1 = 10
 motorB2 = 8
 motorBpwm = 12
 
+OFFSETS = [
+    [0,1],
+    [1,0],
+    [0,-1],
+    [-1,0],
+]
+
+# so increase direction is go right => + is right
+
 #setup
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(motorA1,GPIO.OUT)
@@ -63,57 +72,66 @@ def changeDriveSpeed(speed):
     pwmA.ChangeDutyCycle(driveSpeed)
     pwmB.ChangeDutyCycle(driveSpeed)
 
-def changeTurnSpeed(speed):
-    turnSpeed = speed
-
-
-def stop():
-    GPIO.output(motorA1, GPIO.LOW)
-    GPIO.output(motorA2, GPIO.LOW)
-    GPIO.output(motorB1, GPIO.LOW)
-    GPIO.output(motorB2, GPIO.LOW)
-    pwmA.ChangeDutyCycle(0)
-    pwmB.ChangeDutyCycle(0)
-
-def turn(direction):
-    if direction == 'right':
-        GPIO.output(motorA1, GPIO.LOW)
-        GPIO.output(motorA2, GPIO.HIGH)
-        GPIO.output(motorB1, GPIO.HIGH)
-        GPIO.output(motorB2, GPIO.LOW)
-    elif direction == 'left':
-        GPIO.output(motorA1, GPIO.HIGH)
-        GPIO.output(motorA2, GPIO.LOW)
-        GPIO.output(motorB1, GPIO.LOW)
-        GPIO.output(motorB2, GPIO.HIGH)
-    pwmA.ChangeDutyCycle(turn_speed)
-    pwmB.ChangeDutyCycle(turn_speed)
-
-def turnDegrees(degrees):
-    #magnometer pro
-    #do this later
-    pass
-
 class movement:
     @staticmethod
-    async def start_turn(dir:int) -> None:
+    async def turn(dir:int) -> None:
         '''
-        
+        Turn in the direction specified by `dir`
+        A positive direction is right, negative is left, and 0 is stop
         '''
+
+        if dir > 0:
+            GPIO.output(motorA1, GPIO.LOW)
+            GPIO.output(motorA2, GPIO.HIGH)
+            GPIO.output(motorB1, GPIO.HIGH)
+            GPIO.output(motorB2, GPIO.LOW)
+        elif dir < 0:
+            GPIO.output(motorA1, GPIO.HIGH)
+            GPIO.output(motorA2, GPIO.LOW)
+            GPIO.output(motorB1, GPIO.LOW)
+            GPIO.output(motorB2, GPIO.HIGH)
+        else:
+            await movement.stop()
     
     @staticmethod
     async def drive(dir:int) -> None:
         '''
         
         '''
+        if (dir=='forward'):
+            GPIO.output(motorA1, GPIO.HIGH)
+            GPIO.output(motorA2, GPIO.LOW)
+            GPIO.output(motorB1, GPIO.HIGH)
+            GPIO.output(motorB2, GPIO.LOW)
+
+        elif (dir=='backward'):
+            GPIO.output(motorA1, GPIO.LOW)
+            GPIO.output(motorA2, GPIO.HIGH)
+            GPIO.output(motorB1, GPIO.LOW)
+            GPIO.output(motorB2, GPIO.HIGH)
+        pwmA.ChangeDutyCycle(driveSpeed)
+        pwmB.ChangeDutyCycle(driveSpeed)
 
     @staticmethod
-    async def stop(dir:int) -> None:
+    async def stop() -> None:
         '''
-        
+        Make it stop turning
         '''
+        GPIO.output(motorA1, GPIO.LOW)
+        GPIO.output(motorA2, GPIO.LOW)
+        GPIO.output(motorB1, GPIO.LOW)
+        GPIO.output(motorB2, GPIO.LOW)
+        pwmA.ChangeDutyCycle(0)
+        pwmB.ChangeDutyCycle(0)
 
     @staticmethod
     async def change_turn_speed(to:float) -> None:
         global turn_speed
         turn_speed = to
+
+    @staticmethod
+    async def change_drive_speed(to:float) -> None:
+        global driveSpeed
+        driveSpeed = to
+
+    
